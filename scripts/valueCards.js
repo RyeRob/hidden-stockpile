8ab5bae8152dc0998551da50239d7172cbb71808
JustinCoutinho <justincoutinho@live.ca>
2019-05-27 19:08:54 -0400

    Value Cards Page

---
+++ scripts/valueCards.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  let i = 1;
+  //ADD
+  $(".addCardBtn").click(function() {
+    i++;
+    $("#formRow").append(
+      `<div id="formRow` +
+        i +
+        `"> <input type="text" id="cardName" name="cardName" />
+    <select id="cardSet" name="cardSet" autocomplete="off">
+        <option value="a" selected>A</option>
+        <option value="b">B</option>
+        <option value="c">C</option>
+    </select>
+    <input type="number" id="cardNum" name="cardNum" />
+    <button type="button" id="` +
+    i +
+    `" class="deleteCardBtn btn-floating btn-small waves-effect waves-light red">
+    <i class="fas fa-trash-alt"></i></button>
+</div>`
+    );
+    //DELETE
+    $(".deleteCardBtn").click(function() {
+      let buttonId = $(this).attr("id");
+      $("#formRow" + buttonId).remove();
+    });
+  });
+});
--- views/partials/header.php
+++ views/partials/header.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hidden Stockpile</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="../../styles/main.css">
     <link rel="stylesheet" type="text/css" href="../../styles/header.css">
+    <link rel="stylesheet" type="text/css" href="../../styles/valueCards.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    
 </head>
+
 <body>
