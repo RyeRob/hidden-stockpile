215ed4d2754b1f5ec763c7c10ba1e7fead1d7e01
RyeRob <ryan.robinson5@gmail.com>
2019-06-02 23:43:59 -0400

    styling and nav

--- views/contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-define( 'WEB_ROOT', $_SERVER['DOCUMENT_ROOT'] );
-include( WEB_ROOT.'/views/partials/header.php' );
-include( WEB_ROOT.'/views/partials/menu.php' );
-?>
-
-<h1>Contact Page</h1>
-
-<?php include( WEB_ROOT.'/views/partials/footer.php' ); ?>
